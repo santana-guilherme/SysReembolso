dcd484ed3096f2e6d1c40950a6848fcf7506b727
Guilherme Sant'Ana <guilherme.sant_ana@outlook.com>
2020-10-06 14:30:43 -0300

    add new secret key to specific environment variable file

--- sysreembolso/settings.py
+++ sysreembolso/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'j%j0k4qpz86655l^sgqywdhga89ppj0$_o!e!b%44u*q7@xf(d'
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, 'sysreembolso', '.env') )
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
