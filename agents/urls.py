f66c973248ad3497230d4d9f6f71aed0e56a4df7
Guilherme Sant'Ana <guilherme.sant_ana@outlook.com>
2020-10-26 17:10:18 -0300

    conditional navbar element render

--- agents/urls.py
+++ agents/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
 from . import views
 
+app_name='agents'
 urlpatterns = [
-    path('login', auth_views.LoginView.as_view(template_name='agents/login.html')),
-    path('logout', auth_views.LogoutView.as_view()),
-    path('register', views.registerUser, name='registerName')
+    path('login', auth_views.LoginView.as_view(template_name='agents/login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('register', views.registerUser, name='register')
 ]
