87f40f51e6ef3316ccef8120654382c39c10cd98
Guilherme Sant'Ana <guilherme.sant_ana@outlook.com>
2020-10-28 15:55:00 -0300

    Queues visualization according to user

--- refund/utils.py
+++ refund/utils.py
@@ -2,7 +2,11 @@ def is_member(user: object, group_name: str) -> bool:
     return user.groups.filter(name=group_name).exists()
 
 def is_analyst(user: object) -> bool:
-    return user.groups.filter(name='Analyst').exists()
+    return is_member(user, 'Analyst')
 
 def is_treasurer(user: object) -> bool:
     return is_member(user, 'Treasurer')
+
+
+def its_only(user: object, group_name: str) -> bool:
+    return is_member(user, group_name) and len(user.groups.all()) == 1
