c7b4b5961b5d0a32b2e3400a1ad01339da42b91d
Guilherme Sant'Ana <guilherme.sant_ana@outlook.com>
2020-11-06 18:26:16 -0300

    add solicitation by month

--- refund/static/refund/js/dashboard.js
+++ refund/static/refund/js/dashboard.js
@@ -1,36 +1,51 @@
 $(document).ready(() => {
-    const $user_charts = $('#user_charts')
+    const refunds_by_user = $('#refunds_by_user')
     $.ajax({
-        url: $user_charts.data('url'),
+        url: refunds_by_user.data('url'),
         success: (data) => {
-            var ctx  = $user_charts[0].getContext('2d');
-            new Chart(ctx,{
-                type: 'bar',
-                data: {
-                    labels: data.labels,
-                    datasets: [{
-                        label: 'Refund',
-                        data: data.data,
-                    }]
-                },
-                options: {
-                    responsive: true,
-                    legend:{ position: 'top' },
-                    title: {
-                        display: true,
-                        text: 'Refunds by user'
-                    },
-                    scales: {
-                        yAxes: [{ ticks: { min: 0 } }]
-                    },
-                    plugins: {
-                        colorschemes: {
-                            scheme: 'brewer.SetOne3'
-                        }
-                    }
-                }
-            })
+           barChart(data, refunds_by_user)
+        }
+    })
+
+    const solicitations_by_date = $('#solicitations_by_month')
+    $.ajax({
+        url:  solicitations_by_date.data('url'),
+        success: (data) => {
+            barChart(data, solicitations_by_date)
         }
     })
 })
 
+function barChart(data, el) {
+    var ctx  = el[0].getContext('2d');
+    new Chart(ctx,{
+        type: 'bar',
+        data: {
+            labels: data.labels,
+            datasets: [{
+                label: 'Refund',
+                data: data.data,
+            }]
+        },
+        options: {
+            responsive: true,
+            legend:{ position: 'top' },
+            title: {
+                display: true,
+                text: 'Refunds by user'
+            },
+            scales: {
+                yAxes: [{ ticks: { min: 0 } }]
+            },
+            plugins: {
+                colorschemes: {
+                    scheme: 'brewer.Paired12'
+                }
+            },
+            layout: {
+                width: 300,
+                height: 300
+            }
+        }
+    })
+}
--- refund/views/chart_views.py
+++ refund/views/chart_views.py
@@ -3,11 +3,12 @@ from django.db.models.aggregates import Sum
 from django.http import JsonResponse
 from django.db import connection
 from django.db.models import Count
+from django.utils.functional import LazyObject
 from refund.models import AnalysisQueue, FinishedQueue, PaymentQueue, \
     Solicitation, RefundBundle, ItemSolicitation
 
 
-def refund_by_user(request):
+def refunds_by_user(request):
     refund_queryset = RefundBundle.objects.values(
         'user__first_name', 'user__last_name').annotate(
             total_refund=Sum('price'))
@@ -16,6 +17,19 @@ def refund_by_user(request):
     for entry in refund_queryset:
         labels.append(entry['user__first_name'] + ' ' + entry['user__last_name'])
         data.append(entry['total_refund'])
+    return JsonResponse(data={
+        'labels': labels,
+        'data': data
+    }, safe=False)
+
+def solicitations_by_month(request):
+    refunds = Solicitation.objects.values(
+        'created__month').annotate(Sum('price'))
+    labels = ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez']
+    data = [0]*12
+    for entry in refunds:
+        data[entry['created__month']-1] = entry['price__sum']
+
     return JsonResponse(data={
         'labels': labels,
         'data': data
